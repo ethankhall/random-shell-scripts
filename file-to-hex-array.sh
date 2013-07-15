aa962cf5bf24fa7977545d77a516dadfa79352bf
Ethan Hall <ethan@ehdev.io>
2013-07-15 12:58:20 -0500

    Update file-to-hex-array.sh

--- file-to-hex-array.sh
+++ file-to-hex-array.sh
@@ -7,5 +7,7 @@ if [ $# -ne 1 ]; then
 fi
 
 file_name=$1
-echo "$file_name converts to: "
-hexdump $file_name | sed -e 's/\([0-9a-f]\)\{3,\}//g;s/ /, 0x/g;s/0x, //g' | tr '\n' ' ' | cut -c 3-
+echo "$file_name converts to:"
+echo "{"
+hexdump $file_name | sed -e 's/\([0-9a-f]\)\{3,\}//g;s/ /, 0x/g;s/0x, //g' | tr '\n' ' ' | cut -c 3- | rev | cut -b 7- | rev
+echo "}"
