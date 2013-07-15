d153ccc539743e2099165f20dc027b5c01a1639d
Ethan Hall <ethan@ehdev.io>
2013-07-15 12:45:59 -0500

    Create file-to-hex-array.sh

---
+++ file-to-hex-array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Take a file, convert it into a hex array
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 file_name"
+  exit -1
+fi
+
+file_name=$1
+echo "$file_name converts to: "
+hexdump $file_name | sed -e 's/\([0-9a-f]\)\{3,\}//g;s/ /, 0x/g;s/0x, //g' | tr '\n' ' ' | cut -c 3-
